ed35fc374bcb2a7b122deefb594a5ea296cb6e81
duckien <nguyenduckien0162@gmail.com>
2024-05-22 09:03:14 +0700

    init

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Home from './src/Home'
+import Detail from './src/Detail'
+import Add from './src/Add'
+  
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Detail" component={Detail} />
+          <Stack.Screen name="Add" component={Add} />
+          
+        </Stack.Navigator>
+      </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native"
+
+const st = StyleSheet.create({
+    khungDSSP:{
+        backgroundColor:"yellow",
+        flex:1
+    },
+    itemPro:{
+        margin:5,
+        backgroundColor: "cyan"
+    }
+});
+
+export default st;
